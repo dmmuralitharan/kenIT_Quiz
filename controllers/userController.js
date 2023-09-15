4ddf36de3790be7ba7d5da3750b4a60a9456d1ae
dmmuralitharan <dmmuralitharan@gmail.com>
2023-09-15 12:12:21 +0530

    removing all comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,6 @@ exports.insertInfo = async (req, res) => {
   const { participant1, participant2, clgname } = req.body;
   try {
     const username = req.session.userName;
-    // console.log(username);
     const newInfo = await Info.create({
       participant1,
       participant2,
@@ -44,8 +43,6 @@ exports.allQuestions = async (req, res) => {
     const questions = await Questions.find();
     const sessionName = req.session.userName;
 
-    // timerTime = startTimer(10);
-
     res.render("quiz", { questions, sessionName });
   } catch (err) {
     console.error("Error fetching questions:", err);
@@ -58,7 +55,7 @@ exports.checkAnswer = async (req, res) => {
     let count = 0;
     const questions = await Questions.find();
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 25; i++) {
       const answerIndex = parseInt(req.body[`answer${i}`]);
       if (answerIndex === questions[i].correctOption) {
         count = count + 1;
@@ -67,35 +64,24 @@ exports.checkAnswer = async (req, res) => {
 
     const username = req.session.userName;
     const TimeString = req.body.hTimer;
-    console.log(TimeString);
     const time1Str = "15:00";
     const time2Str = TimeString; 
     
-    // Function to convert time strings to total seconds
     function timeToSeconds(timeStr) {
       const [minutesPart, secondsPart] = timeStr.split(/[:.]/).map(Number);
       return minutesPart * 60 + secondsPart;
     }
     
-    
-    
     const time1Seconds = timeToSeconds(time1Str);
     const time2Seconds = timeToSeconds(time2Str);
-    console.log(time1Seconds);
-    console.log(time2Seconds);
     
     const timeDifferenceSeconds = Math.abs(time1Seconds - time2Seconds);
-    console.log(timeDifferenceSeconds);
     
     const minutesDifference = Math.floor(timeDifferenceSeconds / 60);
-    console.log(minutesDifference);
     const secondsDifference = timeDifferenceSeconds % 60;
-    console.log(secondsDifference);
     
     const formattedDifference = `${minutesDifference}:${secondsDifference}`;
     
-    console.log(`${formattedDifference}`);
-
     const newResult = await QuizResult.create({
       username,
       answerCount: count,
