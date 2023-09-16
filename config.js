ccb40d047983ff5d04bef43725376f00688e7893
dmmuralitharan <dmmuralitharan@gmail.com>
2023-09-16 21:30:29 +0530

    add function -> right click unavailable

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // config.js
 module.exports = {
-  HOSTIP: "127.0.0.1" ,
+  HOSTIP: "127.0.0.1",
   // HOSTIP: "192.168.1.200" ,
-  PORT: 3500,
+  // HOSTIP: "192.168.15.97" ,
+  PORT: 3535,
   mongoURI: "mongodb://127.0.0.1:27017/testquiz",
   sessionSecret:
     "320874c770291166e091135be589fda9ee4b71e9327ffe59533196dbde424922",
