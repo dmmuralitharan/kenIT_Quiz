6c935a54f9cd185b516f000d2157f7facb161393
MURALITHARAN D <dmmuralitharan@gmail.com>
2024-01-12 22:45:37 +0530

    v1

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// router.get('/', (req, res) => {
-//   res.render('index');
-// });
-
-router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../public/index.html");
+router.get('/', (req, res) => {
+  res.render('index');
 });
+
+// router.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/../public/index.html");
+// });
 
 router.get("/login", (req, res) => {
     res.render("login");
@@ -17,7 +17,7 @@ router.get("/login", (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.userId) {
-    res.render('/personalinfo');
+    res.render('personalinfo');
   } else {
     res.render('login');
   }
